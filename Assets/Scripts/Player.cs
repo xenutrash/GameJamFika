779aead0acb84441840283faeae088b7f2ba5d6b
Linusvall <linusvallgren@gmail.com>
2024-10-08 22:19:37 +0200

    Drifting version 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,28 @@
-
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-
-    private Gamepad Controller;
     public PlayerStats Stats;
-    
     public float currentSpeed { get; private set; }
     public bool AllowMovement { get; set; } = false;
     private Rigidbody rb;
     public CamerController camerController;
 
-    public float turnRate;
-    private bool isDrifting;
-    [SerializeField] private float extraDrift;
+    private float speedBoost = 0;
+    private bool isDrifting = false;
+    private float originalTurnRate;
 
-    float speedBoost = 0; 
-
-    GameObject playerModel; 
     // Start is called before the first frame update
     void Start()
     {
-
-        // Add The Mesh here 
-        //playerModel = Instantiate(Stats.Object);
-        //playerModel.transform.parent = gameObject.transform;
         AllowMovement = true;
         if (rb == null)
         {
             rb = gameObject.GetComponent<Rigidbody>();
         }
-        //camerController.SetTarget(transform);
 
-
+        originalTurnRate = Stats.turnRate;
     }
 
     // Update is called once per frame
@@ -42,131 +30,93 @@ public class Player : MonoBehaviour
     {
         if (!AllowMovement)
         {
-            return; 
+            return;
         }
 
-        if (isDrifting)
-        {
-            turnRate = Stats.turnRate + extraDrift;
-           
-        }
-        else
-        {
-            turnRate = Stats.turnRate;
-        }
+        float turn = GetKeyboardInput();
 
-        float turn = Controller == null ? GetKeyboardInput() : GetControllerInput();
-
-        // Apply movement
-        if (currentSpeed >= Stats.maxSpeed + speedBoost)
+        if (!isDrifting)
         {
-            if (isDrifting)
+            if (currentSpeed >= Stats.maxSpeed + speedBoost)
             {
-                currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost - 3, Stats.acceleration + speedBoost * Time.deltaTime);
+                currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost, Stats.acceleration * Time.deltaTime);
             }
-            else
+
+            if (currentSpeed < Stats.maxSpeed + speedBoost)
             {
-               currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost, Stats.acceleration + speedBoost * Time.deltaTime);
+                currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost, Stats.acceleration * Time.deltaTime);
             }
         }
 
-        if (currentSpeed < Stats.maxSpeed + speedBoost)
-        {
-            if (isDrifting)
-            {
-                currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost - 3, Stats.acceleration + speedBoost * Time.deltaTime);
-            }
-            else
-            {
-                currentSpeed = Mathf.Lerp(currentSpeed, Stats.maxSpeed + speedBoost, Stats.acceleration + speedBoost * Time.deltaTime);
-            }
-        }
-
-        Vector3 vel = transform.forward * currentSpeed; 
+        Vector3 vel = transform.forward * currentSpeed;
         vel.y = rb.velocity.y;
         rb.velocity = vel;
-        //transform.position = new(transform.position.x, transform.position.y, transform.position.z + currentSpeed * Time.deltaTime);
-        
-        transform.transform.eulerAngles = new Vector3(
-        transform.eulerAngles.x,
-        turn,
-        transform.eulerAngles.z
-    );
 
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            turn,
+            transform.eulerAngles.z
+        );
 
-
-
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartDrifting();
+        }
+        if((Input.GetKeyUp(KeyCode.K)))
+        {
+            StopDrifting();
+        }
     }
-
-
-    public void SetOwner(Gamepad gamePad)
-    {
-        Controller = gamePad; 
-    }
-
 
     float GetKeyboardInput()
     {
-
         float turn = transform.eulerAngles.y;
+
         if (Input.GetKey(KeyCode.D))
         {
-            turn += (turnRate * Time.deltaTime);
+            turn += (Stats.turnRate * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            turn -= (turnRate * Time.deltaTime);
+            turn -= (Stats.turnRate * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            isDrifting = true;
-        }
-
-        if(Input.GetKeyUp(KeyCode.K)){
-            isDrifting = false;
-        }
-
-        return turn; 
+        return turn;
     }
 
-
-    float GetControllerInput()
+    void StartDrifting()
     {
-        float turn = transform.localPosition.y;
-
-        if (Controller.leftShoulder.value > 0.5)
+        if (!isDrifting)
         {
-            // Drift 
-        }
+            isDrifting = true;
+            Stats.turnRate *= 2f;
+            currentSpeed *= 0.9f;
 
-        if (Controller.rightStick.value.x > 0.1f)
+            rb.drag = rb.drag * 0.5f; 
+        }
+    }
+
+    void StopDrifting()
+    {
+        if (isDrifting)
         {
-            // move left
-            turn += (Stats.turnRate * Time.deltaTime * Controller.rightStick.value.x);
+            isDrifting = false;
+            Stats.turnRate = originalTurnRate;
+            rb.drag = 3.0f;
 
         }
-
-        if (Controller.rightStick.value.x < -0.1f)
-        {
-            turn -= (Stats.turnRate * Time.deltaTime * Controller.rightStick.value.x);
-        }
-
-        return turn; 
     }
 
     public void SetSpeedBoost(float speedBoost, bool apply = true)
     {
-      
         if (!apply)
         {
             this.speedBoost = 0;
-            return; 
+            return;
         }
+
         Debug.Log("SpeedBoost yay");
-        this.speedBoost = speedBoost * Stats.boostMultiplier; 
-
+        this.speedBoost = speedBoost * Stats.boostMultiplier;
     }
-
 }
