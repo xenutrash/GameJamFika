7bd99587c602ad51cc095144aa7489a13a7b53c8
NoWen22 <nora@norrkross.com>
2024-10-08 18:53:58 +0200

    sound to menu
    added button sounds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    // [SerializeField] AudioClip[] soundClips;
+    // [SerializeField] AudioSource audioSource;
+
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -12,4 +15,20 @@ public class MainMenu : MonoBehaviour {
         Application.Quit();
         Debug.Log("Quit"); //Added this message bcs this wont work in unity
     }
+
+    // public void PlayRandomSound() {
+    //     if (soundClips.Length > 0) {
+    //         // Slumpa fram ett index i soundClips-arrayen
+    //         int randomIndex = Random.Range(0, soundClips.Length);
+
+    //         // Hämta ett slumpat ljudklipp
+    //         AudioClip clip = soundClips[randomIndex];
+
+    //         // Spela ljudet
+    //         audioSource.PlayOneShot(clip);
+    //     }
+    //     else {
+    //         Debug.LogError("Inga ljudklipp är tillagda i soundClips-arrayen.");
+    //     }
+    // }
 }
