8b49fdd6af25b49bda96017c82ecdeb03a16f314
NoWen22 <nora@norrkross.com>
2024-10-09 19:49:44 +0200

    introScene
    1 2 3 GOOOO

---
+++ Assets/Scripts/Intro2DManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Intro2DManager : MonoBehaviour {
+    [SerializeField] private GameObject[] countdownObjects; // Array för varje GameObject (1, 2, 3, GO)
+    [SerializeField] private AudioSource audioS;
+    [SerializeField] private AudioClip[] numberSounds; // Ljudklipp för "1", "2", "3", "GO!"
+    [SerializeField] private string triggerName = "Play"; // Namnet på samma trigger för alla objekten
+
+    [SerializeField] private float countdownInterval = 1f;  // Tidsintervall mellan varje steg i nedräkningen
+    [SerializeField] private float soundCountdown = 2f;
+
+    void Start()
+    {
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        // Trigga samma animation och spela ljud för varje objekt
+        for (int i = 0; i < countdownObjects.Length; i++)
+        {
+            GameObject currentObject = countdownObjects[i];
+            Animator animator = currentObject.GetComponent<Animator>();
+
+            // Spela animationen med samma trigger
+            if (animator != null)
+            {
+                animator.SetTrigger(triggerName);
+            }
+
+            yield return new WaitForSeconds(soundCountdown);
+
+            // Spela upp motsvarande ljud för siffran eller "GO!"
+            if (i < numberSounds.Length)
+            {
+                audioS.PlayOneShot(numberSounds[i]);
+            }
+
+            // Vänta innan nästa animation triggas
+            yield return new WaitForSeconds(countdownInterval);
+        }
+
+        // Här kan du lägga till logik för att starta spelet när nedräkningen är klar
+    }
+}
